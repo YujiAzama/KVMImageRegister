a291917c66687a007c77d435b3371d97041b66e2
Yuji Azama <yuji.azama@gmail.com>
2017-05-17 20:42:43 +0900

    Update

---
+++ README.md
@@ -0,0 +1,67 @@
+# KVMImageRegister
+
+## Overview
+
+KVMImageRegister is the tool for create and define VM that is environment for testing.
+You can create KVM image very easy and fastly if you use KVMImageRegister.
+
+## Usage
+
+You can clone KVMImageRegister as follows:
+
+```bash
+$ git clone https://github.com/YujiAzama/KVMImageRegister.git
+$ cd KVMImageRegister/
+```
+Just execute create.sh as superuser to create new VM.
+
+```bash
+$ sudo ./create.sh
+[Step1] Select base image number from below images.
+  0: ubuntu-16.04.02-server-amd64
+Base Image: 0
+
+[Step2] Input new VM name.
+  Used names: []
+Image name: myvm
+
+[Step3] Input memory size of new VM.
+Memory size [GB]: 4
+
+[Step4]Input VNC port number of new VM.
+  Used Ports: []
+VPC Port: 5900
+
+Creating VM...
+Domain myvm defined from myvm.xml
+
+Domain myvm started
+
+
+Image creating is Successful. New VM Info is below:
+
+  Create Date : 2017-05-17 17:16:03
+  MAC Address : 52:54:00:0d:b5:3b
+  IP Address  : 192.168.122.190/24
+  Host Name   : ubuntu
+
+$
+$ ssh ubuntu@192.168.122.190
+The authenticity of host '192.168.122.190 (192.168.122.190)' can't be established.
+ECDSA key fingerprint is SHA256:D59TTXIWlXdVPsNPPR2jnN9pMUrt4Vbn4FtF8oXSHhU.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added '192.168.122.190' (ECDSA) to the list of known hosts.
+ubuntu@192.168.122.190's password:   # Default password is "password".
+Welcome to Ubuntu 16.04.2 LTS (GNU/Linux 4.4.0-62-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+76 packages can be updated.
+32 updates are security updates.
+
+
+Last login: Mon May 15 09:18:01 2017
+ubuntu@ubuntu:~$
+```
--- create.sh
+++ create.sh
@@ -7,8 +7,22 @@ fi
 
 # Start creating image
 
-# Image name
-echo -n "Used names: "
+# Select base image
+echo "[Step1] Select base image number from below images."
+BASE_IMAGES=(`echo $(ls images/)`)
+cnt=0
+for image in ${BASE_IMAGES[@]}; do
+  echo "  "${cnt}: ${image}
+  let cnt++
+done
+echo -n "Base Image: "
+read NUM
+BASE_IMAGE=${BASE_IMAGES[${NUM}]}
+echo
+
+# New image name
+echo "[Step2] Input new VM name."
+echo -n "  Used names: "
 LIST=$(virsh list --all | tail -n +3 | awk '{print $2}')
 arr=(`echo $LIST`)
 echo -n "["
@@ -21,14 +35,18 @@ echo -n "Image name: "
 read IMAGE_NAME
 cp base.xml ${IMAGE_NAME}.xml
 sed -i -e "s/image-name/${IMAGE_NAME}/g" ${IMAGE_NAME}.xml
+echo
 
 # Memory size
+echo "[Step3] Input memory size of new VM."
 echo -n "Memory size [GB]: "
 read MEMORY_SIZE
 sed -i -e "s/memory-size/$((1024*1024*${MEMORY_SIZE}))/g" ${IMAGE_NAME}.xml
+echo
 
 # VNC Port
-echo -n "Used Ports: ["
+echo "[Step4]Input VNC port number of new VM."
+echo -n "  Used Ports: ["
 for item in ${arr[@]}; do
   echo -n $(virsh dumpxml $item | grep vnc | awk '{print $3}' | sed -e "s/port=//g" -e "s/'//g")", "
 done
@@ -36,19 +54,30 @@ echo "]"
 echo -n "VPC Port: "
 read VNC_PORT
 sed -i -e "s/vnc-port/${VNC_PORT}/g" ${IMAGE_NAME}.xml
+echo
 
 # Create base image from splite pieces.
 echo "Creating VM..."
 mkdir -p /opt/libvirt/images
-cat image/image-piece.* > /opt/libvirt/images/${IMAGE_NAME}.qcow2
+cat images/${BASE_IMAGE}/image-piece.* > /opt/libvirt/images/${IMAGE_NAME}.qcow2
 
 virsh define ${IMAGE_NAME}.xml
 virsh start ${IMAGE_NAME}
 
 sleep 10
 
-MAC=$(virsh dumpxml test | grep "mac address" | awk '{print $2}' | sed -e "s/address='//g" -e "s#'/>##g")
+MAC=$(virsh dumpxml ${IMAGE_NAME} | grep "mac address" | awk '{print $2}' | sed -e "s/address='//g" -e "s#'/>##g")
 
-echo "Image creating is Successful."
-echo "VM Info"
-virsh net-dhcp-leases default | grep $MAC
+echo "Image creating is Successful. New VM Info is below:"
+RES=$(virsh net-dhcp-leases default | grep $MAC)
+arr=($RES)
+CREATE_DATE=${arr[0]}" "${arr[1]}
+MAC_ADDR=${arr[2]}
+IP_ADDR=${arr[4]}
+HOST_NAME=${arr[5]}
+echo
+echo "  Create Date : "${CREATE_DATE}
+echo "  MAC Address : "${MAC_ADDR}
+echo "  IP Address  : "${IP_ADDR}
+echo "  Host Name   : "${HOST_NAME}
+echo
