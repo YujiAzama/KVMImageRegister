47d7b08a6954a57ff9af25610d35d8746f445ee5
Yuji Azama <yuji.azama@gmail.com>
2018-01-17 08:42:24 +0900

    Add delete VM script

---
+++ delete.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ${EUID:-${UID}} != 0 ]; then
+    echo 'Not superuser.'
+    exit
+fi
+
+echo
+IMAGES=(`virsh list --all | tail -n +3 | awk '{print $2}' | grep -v '^\s*$'`)
+cnt=0
+for image in ${IMAGES[@]}; do
+  echo "  "${cnt}: ${image}
+  let cnt++
+done
+echo
+echo -n "Delete image: "
+read NUM
+IMAGE=${IMAGES[${NUM}]}
+
+echo "Deleting VM..."
+virsh destroy ${IMAGE}
+
+virsh undefine ${IMAGE}
+
+echo "Image Deleting is Successful."
